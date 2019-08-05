bdcbab0bcdd1b39d6314e2008bbae586b1253b89
quentinbonnetOC <50270491+quentinbonnetOC@users.noreply.github.com>
2019-08-05 15:08:45 +0200

    first commit

---
+++ adminModel.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Class Admuin
+ */
+class Admin{
+    /**
+     * @param object connexion à la bdd
+     */
+    public function dbConnect(){
+        $db = new PDO('mysql:host=localhost; dbname=projet_4; charset=utf8', 'asul', 'asul');
+        return $db;
+    }
+    /**
+     * @param object selection de tous les admins
+     */
+    public function authentification($idt, $mdp){
+        $db = $this->dbConnect();
+        $req = $db->query('SELECT * FROM admin WHERE idt = "'.$idt.'" AND mdp = "'.$mdp.'"');
+        return $req;
+    }
+    public function createArticle($chapter, $title, $date){
+        $db = $this->dbConnect();
+        $req = $db->query('INSERT INTO `articles`(chapter, title, date) VALUES ("'.$chapter.'", "'.$title.'", "'.$date.'")');
+        return $req;
+    }
+    public function readArticle(){
+        $db = $this->dbConnect();
+        $req = $db->query('SELECT * FROM articles');
+        return $req;
+    }
+    public function deleteArticle($id){
+        $db = $this->dbConnect();
+        $req = $db->query('DELETE FROM `articles` WHERE id="'.$id.'"');
+        return $req;
+    }
+    public function updateArticle($chapter, $title, $date, $id){
+        $db = $this->dbConnect();
+        $req = $db->query('UPDATE `articles` SET chapter = "'.$chapter.'", title = "'.$title.'", date = "'.$date.'" WHERE id = "'.$id.'"');
+        return $req;
+
+    }
+}
