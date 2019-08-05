a3726ad16a40651e7f53b4035f7402ef91b88f3c
quentinbonnetOC <50270491+quentinbonnetOC@users.noreply.github.com>
2019-08-05 16:36:11 +0200

    add foreign key for comments

--- commentaireModel.php
+++ commentaireModel.php
@@ -1,9 +1,12 @@
 <?php
 class Commentaire extends Admin{
-    public function commentaireUtilisateur($nom, $prenom, $email, $commentaire){
+    public function commentaireUtilisateur($article_id, $nom, $prenom, $email, $commentaire){
         $db = $this->dbConnect();
-        $req = $db->query('INSERT INTO `commentaire`(nom, prenom, email, commentaire) VALUES ("'.$nom.'", "'.$prenom.'", "'.$email.'", "'.$commentaire.'")');
+        $req = $db->query('INSERT INTO `commentaire`(article_id, nom, prenom, email, commentaire) VALUES ("'.$article_id.'", "'.$nom.'", "'.$prenom.'", "'.$email.'", "'.$commentaire.'")');
         return $req;
+    }
+    public function readConments(){
+        
     }
 }
 ?>
