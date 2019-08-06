83a988fa5d742c82bfe2f7a3bf866c39a34f536e
quentinbonnetOC <50270491+quentinbonnetOC@users.noreply.github.com>
2019-08-06 17:41:27 +0200

    probleme commentaire

--- utilisateurController.php
+++ utilisateurController.php
@@ -1,21 +1,28 @@
 <?php
 class UtilisateurController{
-    public function readArticleUtilisateur(){
-        $class = new Admin();
-        $readArticle = $class->readArticle();
-        if(isset($_POST['id'])){
-            $this->_commentaireUtilisateur();
-        }
+    public function readArticle(){
+        $classi = new Commentaire();
+        $readArticle = $classi->readArticle();
         require('../app/view/utilisateur.phtml');
+        if(isset($_POST['article_id_createComment'])){
+            $this->createComment();
+        }
+        $this->readComments();
+        
     }
-    private function _commentaireUtilisateur(){
+    private function createComment(){
         $nom = isset($_POST['nom'])? $_POST['nom']:null;
         $prenom = isset($_POST['prenom'])? $_POST['prenom']:null;
         $email = isset($_POST['email'])? $_POST['email']:null;
         $commentaire = isset($_POST['commentaire'])? $_POST['commentaire']:null;
         $article_id = isset($_POST['id'])? $_POST['id'] : null;
         $class = new Commentaire();
-        $commentaireUtilisateur = $class->commentaireUtilisateur($article_id, $nom, $prenom, $email, $commentaire);
+        $createComment = $class->createComment($article_id, $nom, $prenom, $email, $commentaire);
+    }
+    private function readComments(){
+        $classe = new Commentaire();
+        $readComments = $classe->readComments();
+        require('../app/view/utilisateur.phtml');
     }
 }
 ?>  
