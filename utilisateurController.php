b1daaf1cf0133c8a50b9d5af0cf889139a29835b
quentinbonnetOC <50270491+quentinbonnetOC@users.noreply.github.com>
2019-08-08 17:08:52 +0200

    second commit commentaire

--- utilisateurController.php
+++ utilisateurController.php
@@ -7,18 +7,20 @@ class UtilisateurController{
             $this->createComment();
         }
         $class = new Commentaire();
-        $readComments = $class->readComments();
-        
-        require('../app/view/utilisateur.phtml');  
+        $readComments = $class->readComments(); 
+       require('../app/view/utilisateur.phtml');  
     }
     private function createComment(){
         $nom = isset($_POST['nom'])? $_POST['nom']:null;
         $prenom = isset($_POST['prenom'])? $_POST['prenom']:null;
         $email = isset($_POST['email'])? $_POST['email']:null;
         $commentaire = isset($_POST['commentaire'])? $_POST['commentaire']:null;
-        $article_id = isset($_POST['id'])? $_POST['id'] : null;
+        $date = date('d/m/Y');
+        $article_id = isset($_POST['article_id_createComment'])? $_POST['article_id_createComment'] : null;
+        
+
         $class = new Commentaire();
-        $createComment = $class->createComment($article_id, $nom, $prenom, $email, $commentaire);
+        $createComment = $class->createComment($article_id, $nom, $prenom, $email, $commentaire, $date);
     }
 }
 ?>  
