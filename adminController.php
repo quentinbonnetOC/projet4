5bf6e005f794cb59c0b9e4b2e9a9f7d96f2d280f
quentinbonnetOC <50270491+quentinbonnetOC@users.noreply.github.com>
2019-08-13 15:11:56 +0200

    add object AdminController

--- adminController.php
+++ adminController.php
@@ -1,66 +1,72 @@
 <?php
-session_start();
-function authentification(){
-    if($_SESSION['authentification'] == true){
-        readArticle();
-    }else if(!isset($_POST['envoi'])){
-        require('../app/view/authentification.phtml');
-    }else if(isset($_POST['envoi'])&& $_POST['envoi']=='envoyer'){
-        $class = new Admin();
-        $idt = $_POST['idt'];
-        $mdp = $_POST['mdp'];
-        $authentification  = $class->authentification($idt, $mdp);
-        
-        if($authentification->fetch()){
-            $_SESSION['authentification'] = true;
-            readArticle();
-        }else{
-            echo "erreur d'identifiant ou de mot de passe";
-        }
-    }   
-}
-function createArticle(){   
-    if(isset($_POST['envoie']) && $_POST['envoie']=='envoyer'){
-        $class = new Admin();
-        $chapter = $_POST['chapter'];
-        $title = $_POST['title'];        
-        $date = date('d/m/Y');
-        $createArticle = $class->createArticle($chapter, $title, $date);       
+class AdminController{
+    public function authentification(){
+        if($_SESSION['authentification'] == true){
+            $this->readArticle();
+        }else if(!isset($_POST['envoi'])){
+            require('../app/view/authentification.phtml');
+        }else if(isset($_POST['envoi'])){
+            $class = new Admin();
+            $idt = $_POST['idt'];
+            $mdp = $_POST['mdp'];
+            $authentification  = $class->authentification($idt, $mdp);
+            
+            if($authentification->fetch()){
+                $_SESSION['authentification'] = true;
+                $this->readArticle();
+            }else{
+                echo "erreur d'identifiant ou de mot de passe";
+            }
+        }   
     }
-    require('../app/view/createArticle.phtml');
-}
-function readArticle(){
-    if(isset($_SESSION['update']) && $_SESSION['update']  == true){
-        updateArticle();
-    }else{
-        //delete
-        if(!empty($_POST['delete'])){  
-            $id = $_POST['delete'];
-            $testDelete = new Admin;
-            $testDelete->deleteArticle($id);
-        ///delete
-        }else if(!empty($_POST['update'])) {
-            //update
-            require('../app/view/update.phtml');   
-            $_SESSION['update'] = true;
-            updateArticle();
-            ///update
+    public function createArticle(){   
+        if(isset($_POST['envoie']) && $_POST['envoie']=='envoyer'){
+            $class = new Admin();
+            $chapter = $_POST['chapter'];
+            $title = $_POST['title'];        
+            $date = date('d/m/Y');
+            $createArticle = $class->createArticle($chapter, $title, $date);       
+        }
+        require('../app/view/createArticle.phtml');
+    }
+    public function readArticle(){
+        if(isset($_SESSION['update']) && $_SESSION['update']  == true){
+            $this->updateArticle();
         }else{
-            $class = new Admin;
-            $readArticle = $class->readArticle();
-            require('../app/view/backoffice.phtml');
-        }       
-    } 
-}
-function updateArticle(){
-    if(isset($_POST['envoyeur'])){
-        $chapter = $_POST['chapter'];
-        $title = $_POST['title'];
-        $date = $_POST['date'];
-        $id = $_POST['envoyeur'];
-        $testDelete = new Admin;
-        $testDelete->updateArticle($chapter, $title, $date, $id);
-        unset($_SESSION['update']);
-    }  
+            //delete
+            if(!empty($_POST['delete'])){  
+                $id = $_POST['delete'];
+                $testDelete = new Admin;
+                $testDelete->deleteArticle($id);
+            ///delete
+            }else if(!empty($_POST['update'])) {
+                //update
+                require('../app/view/update.phtml');   
+                $_SESSION['update'] = true;
+                $this->updateArticle();
+                ///update
+            }else{
+                $class = new Admin();
+                $readArticle = $class->readArticle();
+                require('../app/view/backoffice.phtml');
+            }       
+        } 
+    }
+    private function updateArticle(){
+        if(isset($_POST['envoyeur'])){
+            $chapter = $_POST['chapter'];
+            $title = $_POST['title'];
+            $date = $_POST['date'];
+            $id = $_POST['envoyeur'];
+            $testDelete = new Admin;
+            $testDelete->updateArticle($chapter, $title, $date, $id);
+            unset($_SESSION['update']);
+        }  
+    }
+    public function signalementCommentAdmin(){
+        $class = new Admin();
+        $signalementCommentAdmin = $class->signalementCommentAdmin();
+        require('../app/view/signalementComment.phtml');
+    }
 }
 ?>
