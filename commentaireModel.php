954d448747b0d5319be68bb170c17a1c0d50a8f5
quentinbonnetOC <50270491+quentinbonnetOC@users.noreply.github.com>
2019-08-13 15:09:11 +0200

    update commentaire table

--- commentaireModel.php
+++ commentaireModel.php
@@ -1,8 +1,8 @@
 <?php
 class Commentaire extends Admin{
-    public function createComment($article_id, $nom, $prenom, $email, $commentaire, $date){
+    public function createComment($article_id, $pseudo, $email, $commentaire, $date){
         $db = $this->dbConnect();
-        $req = $db->query('INSERT INTO `commentaire`(article_id, nom, prenom, email, commentaire, date) VALUES ("'.$article_id.'", "'.$nom.'", "'.$prenom.'", "'.$email.'", "'.$commentaire.'", "'.$date.'")');
+        $req = $db->query('INSERT INTO `commentaire`(article_id, pseudo, email, commentaire, date) VALUES ("'.$article_id.'", "'.$pseudo.'", "'.$email.'", "'.$commentaire.'", "'.$date.'")');
         return $req;
     }
     public function readComments(){
@@ -13,6 +13,11 @@ class Commentaire extends Admin{
     public function readArticle(){
         $db = $this->dbConnect();
         $req = $db->query('SELECT * FROM `articles`');
+        return $req;
+    }
+    public function signalementComment($commentaire_id, $motif){
+        $db = $this->dbConnect();
+        $req = $db->query('INSERT INTO `signalement`(commentaire_id, motif) VALUES ("'.$commentaire_id.'", "'.$motif.'")');
         return $req;
     }
 }
