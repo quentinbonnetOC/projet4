18c2740264f237466517e8c7392a6f73b0edfdd4
quentinbonnetOC <50270491+quentinbonnetOC@users.noreply.github.com>
2019-08-13 15:08:42 +0200

    update commentaire table

--- utilisateurController.php
+++ utilisateurController.php
@@ -5,22 +5,28 @@ class UtilisateurController{
         $readArticle = $class->readArticle();
         if(isset($_POST['article_id_createComment'])){
             $this->createComment();
+        }else if(isset($_POST['signaler'])){
+            $this->signalementComment();
         }
         $class = new Commentaire();
         $readComments = $class->readComments(); 
-       require('../app/view/utilisateur.phtml');  
+        require('../app/view/utilisateur.phtml');  
     }
     private function createComment(){
-        $nom = isset($_POST['nom'])? $_POST['nom']:null;
-        $prenom = isset($_POST['prenom'])? $_POST['prenom']:null;
+        $pseudo = isset($_POST['pseudo'])? $_POST['pseudo']:null;
         $email = isset($_POST['email'])? $_POST['email']:null;
         $commentaire = isset($_POST['commentaire'])? $_POST['commentaire']:null;
         $date = date('d/m/Y');
         $article_id = isset($_POST['article_id_createComment'])? $_POST['article_id_createComment'] : null;
-        
 
         $class = new Commentaire();
-        $createComment = $class->createComment($article_id, $nom, $prenom, $email, $commentaire, $date);
+        $createComment = $class->createComment($article_id, $pseudo, $email, $commentaire, $date);
+    }
+    private function signalementComment(){
+        $commentaire_id = $_POST['commentaire_id'];
+        $motif = $_POST['motif'];
+        $class = new Commentaire();
+        $class->signalementComment($commentaire_id, $motif);
     }
 }
 ?>  
