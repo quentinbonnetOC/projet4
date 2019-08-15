c18a9e5dfc99f987d9a65eac6024cac13647d90f
quentinbonnetOC <50270491+quentinbonnetOC@users.noreply.github.com>
2019-08-15 16:25:58 +0200

    separation model

--- adminModel.php
+++ adminModel.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Class Admuin
+ * Class Admin
  */
 class Admin{
     /**
@@ -16,31 +16,6 @@ class Admin{
     public function authentification($idt){
         $db = $this->dbConnect();
         $req = $db->query('SELECT * FROM admin WHERE idt = "'.$idt.'"');
-        return $req;
-    }
-    public function createArticle($chapter, $title, $date){
-        $db = $this->dbConnect();
-        $req = $db->query('INSERT INTO `articles`(chapter, title, date) VALUES ("'.$chapter.'", "'.$title.'", "'.$date.'")');
-        return $req;
-    }
-    public function readArticle(){
-        $db = $this->dbConnect();
-        $req = $db->query('SELECT * FROM articles');
-        return $req;
-    }
-    public function deleteArticle($id){
-        $db = $this->dbConnect();
-        $req = $db->query('DELETE FROM `articles` WHERE id="'.$id.'"');
-        return $req;
-    }
-    public function updateArticle($chapter, $title, $date, $id){
-        $db = $this->dbConnect();
-        $req = $db->query('UPDATE `articles` SET chapter = "'.$chapter.'", title = "'.$title.'", date = "'.$date.'" WHERE id = "'.$id.'"');
-        return $req;
-    }
-    public function signalementCommentAdmin(){
-        $db = $this->dbConnect();
-        $req = $db->query('SELECT * FROM `signalement`');
         return $req;
     }
 }
