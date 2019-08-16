ce45a8e4ce6691b8ff09fbd5b7a290ee6eda4e6d
quentinbonnetOC <50270491+quentinbonnetOC@users.noreply.github.com>
2019-08-17 00:51:58 +0200

    add redirection

--- utilisateurController.php
+++ utilisateurController.php
@@ -18,15 +18,22 @@ class UtilisateurController{
         $commentaire = isset($_POST['commentaire'])? $_POST['commentaire']:null;
         $date = date('d/m/Y');
         $article_id = isset($_POST['article_id_createComment'])? $_POST['article_id_createComment'] : null;
-
         $class = new Commentaire();
         $createComment = $class->createComment($article_id, $pseudo, $email, $commentaire, $date);
+        ?><script>setTimeout(function(){
+            document.location.href="http://localhost/projet_4/public";
+        },0000);
+        alert("Votre commentaire a bien été enregistré");</script><?php
     }
     private function signalementComment(){
         $commentaire_id = $_POST['commentaire_id'];
         $motif = $_POST['motif'];
         $class = new Commentaire();
         $class->signalementComment($commentaire_id, $motif);
+        ?><script>setTimeout(function(){
+            document.location.href="http://localhost/projet_4/public";
+        },0000);
+        alert("Votre signalement a bien été enregistré");</script><?php
     }
 }
 ?>  
