de54fffc524921ed05c6e17c87e2ee02976146d5
quentinbonnetadthink <quentin.bonnet@nfty.inc>
2023-12-24 12:09:12 +0100

    24/12/2023

--- app/Autoloader.php
+++ app/Autoloader.php
@@ -2,10 +2,9 @@
 namespace App;
 class Autoloader{
     static function autoload($class_name){
-        require __DIR__ . '\\model' . '\\' . $class_name . 'Model.php';
+        require __DIR__ . '/model' . '/' . $class_name . 'Model.php';
     }
     static function register(){
         spl_autoload_register(array(__CLASS__, 'autoload'));
     }
 }
-?>
---
+++ app/model/FormModel.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Class Form
+ * Permet de générer le formulaire de connection au backoffice
+ */
+class Form{
+     /**
+     * @param $index string Index de la valeur à récupérer
+     * @return string
+     */
+    private function getValue($index){
+        return isset($this->data[$index]) ? $this->data[$index] : null;
+    }
+     /**
+     * @param $name string
+     * @return string
+     */
+    public function input($name, $type, $id, $value){
+        return '<input id="'.$id.'" type="'.$type.'" name="'.$name.'" value="'.$value.'">';
+    } 
+     /**
+     * @return string
+     */
+    public function submit($id){
+        return '<button id="'.$id.'" type="submit">Envoyer</button>'
+        ;
+    }
+    public function label($name){
+        return '<label for="'.$name.'">'.$name.'</label>';
+    }
+    public function checkbox($name, $id, $value, $checked){
+        return '<input id="'.$id.'" type="checkbox" name="'.$name.'" value="'.$value.'" '.$checked.'>';
+    }
+}
